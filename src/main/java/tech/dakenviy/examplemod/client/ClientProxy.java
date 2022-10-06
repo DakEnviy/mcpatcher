12e495a853907411cef5719d67d4c266239cb31f
Mikhail Golbakh <golbah@yandex.ru>
2022-10-06 22:05:35 +0300

    initial commit

---
+++ src/main/java/tech/dakenviy/examplemod/client/ClientProxy.java
@@ -0,0 +1,28 @@
+package tech.dakenviy.examplemod.client;
+
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import tech.dakenviy.examplemod.common.CommonProxy;
+
+@SideOnly(Side.CLIENT)
+public class ClientProxy extends CommonProxy {
+
+    @Override
+    public void preInit(FMLPreInitializationEvent event) {
+        super.preInit(event);
+    }
+
+    // Register renderers
+    @Override
+    public void init(FMLInitializationEvent event) {
+        super.init(event);
+    }
+
+    @Override
+    public void postInit(FMLPostInitializationEvent event) {
+        super.postInit(event);
+    }
+}
---
+++ src/main/java/tech/dakenviy/examplemod/common/CommonProxy.java
@@ -0,0 +1,34 @@
+package tech.dakenviy.examplemod.common;
+
+import cpw.mods.fml.common.event.*;
+
+public class CommonProxy {
+
+    // Read config, create and register blocks, items, entity, tile entity, ore dictionary
+    public void preInit(FMLPreInitializationEvent event) {
+    }
+
+    // Register structure
+    public void init(FMLInitializationEvent event) {
+    }
+
+    // Register recipes, handle interaction with other mods
+    public void postInit(FMLPostInitializationEvent event) {
+    }
+
+    public void serverAboutToStart(FMLServerAboutToStartEvent event) {
+    }
+
+    // Register commands
+    public void serverStarting(FMLServerStartingEvent event) {
+    }
+
+    public void serverStarted(FMLServerStartedEvent event) {
+    }
+
+    public void serverStopping(FMLServerStoppingEvent event) {
+    }
+
+    public void serverStopped(FMLServerStoppedEvent event) {
+    }
+}
